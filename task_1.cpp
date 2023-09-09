ca2f34431ba170aeb91fdf4fd34d276a4278a469
VadimS20 <vadim_2014_pushkin@mail.ru>
2023-09-09 21:40:08 +0300

    1

---
+++ task_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int NumberOfVariants;
+	string Name;
+	cout << "Write your name: ";
+	getline(cin,Name);
+	cout << "Write how many variants do you have: ";
+	cin >> NumberOfVariants;
+
+
+	cout << "Your answer is: " << int(Name[0])%NumberOfVariants;
+	return 0;
+}
